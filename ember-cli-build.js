3cae1afccc3081c284592f9af19d3466690d1048
keanedawg <cameronfife@hotmail.com>
2019-10-20 12:27:33 -0500

    added in SCSS support for bootstrap 4

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,7 @@ module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     'ember-bootstrap': {
       'bootstrapVersion': 4,
-      'importBootstrapCSS': true
+      'importBootstrapCSS': false
     },
     sassOptions: {
       extension: 'scss'
