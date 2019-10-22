f858c45cd5caa8cbc236593262c9d4c76b3102be
keanedawg <cameronfife@hotmail.com>
2019-10-21 21:06:44 -0500

    get the data using a simple fetch

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch'; 
 
 export default Route.extend({
+    model() {
+        return fetch('https://hotsapi.net/api/v1/heroes').then(function(response) {
+            console.log(response)
+            return response.json();
+        });
+    }
 });
