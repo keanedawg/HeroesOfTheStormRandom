dc39bbc961e45610afd9028040c64206a67e4e0e
keanedawg <cameronfife@hotmail.com>
2019-10-22 20:13:52 -0500

    removed console log

--- app/routes/home.js
+++ app/routes/home.js
@@ -4,7 +4,6 @@ import fetch from 'fetch';
 export default Route.extend({
   model() {
     return fetch('https://hotsapi.net/api/v1/heroes').then(function (response) {
-      console.log(response)
       return response.json();
     });
   }
