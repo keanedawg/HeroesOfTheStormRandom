3dde0e384be8b037178486859f3a5ecb66281194
keanedawg <cameronfife@hotmail.com>
2019-10-24 21:45:37 -0500

    created event for on click

--- app/components/random-hero-selector.js
+++ app/components/random-hero-selector.js
@@ -3,9 +3,11 @@ import Component from '@ember/component';
 export default Component.extend({
   hero: "Arthas",
   portrait: "http://s3.hotsapi.net/img/heroes/92x93/arthas.png",
+  heroList: [],
   actions: {
     generateRandomHero() {
-      this.login.deleteUser();
+      console.log("something")
+      console.log(heroList)
     }
   }
 });
