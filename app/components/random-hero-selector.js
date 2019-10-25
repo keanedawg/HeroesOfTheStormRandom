24365357a707033e6cc111ac55172984661f2985
keanedawg <cameronfife@hotmail.com>
2019-10-24 21:39:49 -0500

    portrait and name are component variables

--- app/components/random-hero-selector.js
+++ app/components/random-hero-selector.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  hero: "Arthas",
+  portrait: "http://s3.hotsapi.net/img/heroes/92x93/arthas.png",
+  actions: {
+    generateRandomHero() {
+      this.login.deleteUser();
+    }
+  }
 });
