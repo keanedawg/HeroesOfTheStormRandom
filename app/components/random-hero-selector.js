f137f65946bac23e291025c18436e60395520fbc
keanedawg <cameronfife@hotmail.com>
2019-11-05 23:26:23 -0600

    added heroes json file and removed all others

--- app/components/random-hero-selector.js
+++ app/components/random-hero-selector.js
@@ -1,11 +1,21 @@
 import Component from '@ember/component';
+//import heroesTalents from 'heroesTalents';
+//const HeroesTalents = require('heroes-talents')
 
 export default Component.extend({
   hero: "Arthas",
   portrait: "http://s3.hotsapi.net/img/heroes/92x93/arthas.png",
-  heroList: [],
+  heroList: null,
   actions: {
-    generateRandomHero() {
+    async generateRandomHero() {
+      try {
+        const heroes = await heroesTalents.loadHeroJSONFiles()
+        const favoriteHero = heroes.alarak
+        console.log(`My favorite hero is ${favoriteHero.name}. He is an ${favoriteHero.role}.`)
+      } catch (err) {
+        console.log(err)
+      }
+
       let randnum = Math.floor((Math.random() * this.heroList.length) + 1);
       console.log(randnum);
       this.set('hero', this.heroList[randnum]['name']);
