8ef64d2edab5f30abd30bcb7a6943a12dc9c81d2
keanedawg <cameronfife@hotmail.com>
2019-11-10 17:03:58 -0600

    try to dynamically generate some pieces of data

--- app/components/talent-row.js
+++ app/components/talent-row.js
@@ -1,5 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  tagName: "tr"
+  tagName: "tr",
+  heroTalent: null,
+  index: null,
+  init() {
+    this._super(...arguments);
+    let randnum = Math.floor((Math.random() * this.heroTalent.length()) + 1);
+    this.index = randnum;
+  }
 });
