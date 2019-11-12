0b35741a5b02fc2e101a8132467120f1e71bf66a
keanedawg <cameronfife@hotmail.com>
2019-11-12 00:20:35 -0600

    used this.set to allow hero abilities to change on second click

--- app/components/talent-row.js
+++ app/components/talent-row.js
@@ -2,12 +2,13 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: "tr",
+  heroTalentList: null,
   heroTalent: null,
   level: null,
   index: null,
   didReceiveAttrs() {
     this._super(...arguments);
-    this.index = Math.floor((Math.random() * this.heroTalent.length));
-    this.heroTalent = this.heroTalent[this.index];
+    this.set('index', Math.floor((Math.random() * this.heroTalentList.length)));
+    this.set('heroTalent', this.heroTalentList[this.index]);
   }
 });
